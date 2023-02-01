f4adc4b90a4a44a7518c97ddf4f5f50aa28f5488
zulnurdiana <zulnurdiana@gmail.com>
2023-02-01 14:52:57 +0700

    font event

--- src/components/ListEvent.js
+++ src/components/ListEvent.js
@@ -24,67 +24,7 @@ const ListEvent = () => {
 
               <div class="px-4 py-6">
                 <h3 class="font-bold text-dark hover:text-primary text-2xl mb-5 truncate">
-                  Permainan Engklek
-                </h3>
-                <p class="text-lg font-semibold text-primary">
-                  <span>🕓</span> Today at 10:00 PM
-                </p>
-                <p class="text-lg font-semibold text-slate-400 mb-6">
-                  <span>📌</span> Bandung, West Java
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="xl:w-1/4 px-4">
-            <div class="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
-              <div class="relative">
-                <img
-                  src="https://source.unsplash.com/300x200?color"
-                  alt="game"
-                  class="w-full"
-                />
-                <a
-                  href="!#"
-                  target="_blank"
-                  class="absolute -bottom-4 border-2 border-white right-0  w-10 h-10 mr-3 flex justify-center items-center rounded-full bg-primary hover:border-primary text-white"
-                >
-                  <span>🤍</span>
-                </a>
-              </div>
-
-              <div class="px-4 py-6">
-                <h3 class="font-bold text-dark hover:text-primary text-2xl mb-5 truncate">
-                  Permainan Engklek
-                </h3>
-                <p class="text-lg font-semibold text-primary">
-                  <span>🕓</span> Today at 10:00 PM
-                </p>
-                <p class="text-lg font-semibold text-slate-400 mb-6">
-                  <span>📌</span> Bandung, West Java
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="xl:w-1/4 px-4">
-            <div class="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
-              <div class="relative">
-                <img
-                  src="https://source.unsplash.com/300x200?color"
-                  alt="game"
-                  class="w-full"
-                />
-                <a
-                  href="!#"
-                  target="_blank"
-                  class="absolute -bottom-4 border-2 border-white right-0  w-10 h-10 mr-3 flex justify-center items-center rounded-full bg-primary hover:border-primary text-white"
-                >
-                  <span>🤍</span>
-                </a>
-              </div>
-
-              <div class="px-4 py-6">
-                <h3 class="font-bold text-dark hover:text-primary text-2xl mb-5 truncate">
-                  Permainan Engklek
+                  Lorem Ipsum
                 </h3>
                 <p class="text-lg font-normal text-primary">
                   <span>🕓</span> Today at 10:00 PM
@@ -114,12 +54,72 @@ const ListEvent = () => {
 
               <div class="px-4 py-6">
                 <h3 class="font-bold text-dark hover:text-primary text-2xl mb-5 truncate">
-                  Permainan Engklek
+                  Lorem Ipsum
                 </h3>
-                <p class="text-lg font-semibold text-primary">
+                <p class="text-lg font-normal text-primary">
                   <span>🕓</span> Today at 10:00 PM
                 </p>
-                <p class="text-lg font-semibold text-slate-400 mb-6">
+                <p class="text-lg font-normal text-slate-400 mb-6">
+                  <span>📌</span> Bandung, West Java
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="xl:w-1/4 px-4">
+            <div class="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
+              <div class="relative">
+                <img
+                  src="https://source.unsplash.com/300x200?color"
+                  alt="game"
+                  class="w-full"
+                />
+                <a
+                  href="!#"
+                  target="_blank"
+                  class="absolute -bottom-4 border-2 border-white right-0  w-10 h-10 mr-3 flex justify-center items-center rounded-full bg-primary hover:border-primary text-white"
+                >
+                  <span>🤍</span>
+                </a>
+              </div>
+
+              <div class="px-4 py-6">
+                <h3 class="font-bold text-dark hover:text-primary text-2xl mb-5 truncate">
+                  Lorem Ipsum
+                </h3>
+                <p class="text-lg font-normal text-primary">
+                  <span>🕓</span> Today at 10:00 PM
+                </p>
+                <p class="text-lg font-normal text-slate-400 mb-6">
+                  <span>📌</span> Bandung, West Java
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="xl:w-1/4 px-4">
+            <div class="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
+              <div class="relative">
+                <img
+                  src="https://source.unsplash.com/300x200?color"
+                  alt="game"
+                  class="w-full"
+                />
+                <a
+                  href="!#"
+                  target="_blank"
+                  class="absolute -bottom-4 border-2 border-white right-0  w-10 h-10 mr-3 flex justify-center items-center rounded-full bg-primary hover:border-primary text-white"
+                >
+                  <span>🤍</span>
+                </a>
+              </div>
+
+              <div class="px-4 py-6">
+                <h3 class="font-bold text-dark hover:text-primary text-2xl mb-5 truncate">
+                  Lorem Ipsum
+                </h3>
+                <p class="text-lg font-normal text-primary">
+                  <span>🕓</span> Today at 10:00 PM
+                </p>
+                <p class="text-lg font-normal text-slate-400 mb-6">
                   <span>📌</span> Bandung, West Java
                 </p>
               </div>
