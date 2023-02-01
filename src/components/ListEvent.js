d18a58c40138177b4b157716df584336a4d8ab11
zulnurdiana <zulnurdiana@gmail.com>
2023-02-01 14:46:17 +0700

    redesign event feature

--- src/components/ListEvent.js
+++ src/components/ListEvent.js
@@ -7,72 +7,120 @@ const ListEvent = () => {
         <div class="flex flex-wrap px-4">
           <div class="xl:w-1/4 px-4">
             <div class="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
-              <img
-                src="https://source.unsplash.com/300x200?orange"
-                alt="game"
-                class="w-full"
-              />
-              <div class="px-4 py-6">
-                <h3 class="font-bold text-dark hover:text-primary text-xl mb-5 truncate">
-                  Permainan Congklak
-                </h3>
-                <p class="text-base text-secondary mb-8">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Architecto ab fuga enim?
-                </p>
+              <div class="relative">
+                <img
+                  src="https://source.unsplash.com/300x200?color"
+                  alt="game"
+                  class="w-full"
+                />
+                <a
+                  href="!#"
+                  target="_blank"
+                  class="absolute -bottom-4 border-2 border-white right-0  w-10 h-10 mr-3 flex justify-center items-center rounded-full bg-primary hover:border-primary text-white"
+                >
+                  <span>🤍</span>
+                </a>
               </div>
-            </div>
-          </div>
-          <div class="xl:w-1/4 px-4">
-            <div class="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
-              <img
-                src="https://source.unsplash.com/300x200?color"
-                alt="game"
-                class="w-full"
-              />
+
               <div class="px-4 py-6">
-                <h3 class="font-bold text-dark hover:text-primary text-xl mb-5 truncate">
+                <h3 class="font-bold text-dark hover:text-primary text-2xl mb-5 truncate">
                   Permainan Engklek
                 </h3>
-                <p class="text-base text-secondary mb-8">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Architecto ab fuga enim?
+                <p class="text-lg font-semibold text-primary">
+                  <span>🕓</span> Today at 10:00 PM
+                </p>
+                <p class="text-lg font-semibold text-slate-400 mb-6">
+                  <span>📌</span> Bandung, West Java
                 </p>
               </div>
             </div>
           </div>
           <div class="xl:w-1/4 px-4">
             <div class="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
-              <img
-                src="https://source.unsplash.com/300x200?game"
-                alt="game"
-                class="w-full"
-              />
+              <div class="relative">
+                <img
+                  src="https://source.unsplash.com/300x200?color"
+                  alt="game"
+                  class="w-full"
+                />
+                <a
+                  href="!#"
+                  target="_blank"
+                  class="absolute -bottom-4 border-2 border-white right-0  w-10 h-10 mr-3 flex justify-center items-center rounded-full bg-primary hover:border-primary text-white"
+                >
+                  <span>🤍</span>
+                </a>
+              </div>
+
               <div class="px-4 py-6">
-                <h3 class="font-bold text-dark hover:text-primary text-xl mb-5 truncate">
-                  Permainan Gundu
+                <h3 class="font-bold text-dark hover:text-primary text-2xl mb-5 truncate">
+                  Permainan Engklek
                 </h3>
-                <p class="text-base text-secondary mb-8">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Architecto ab fuga enim?
+                <p class="text-lg font-semibold text-primary">
+                  <span>🕓</span> Today at 10:00 PM
+                </p>
+                <p class="text-lg font-semibold text-slate-400 mb-6">
+                  <span>📌</span> Bandung, West Java
                 </p>
               </div>
             </div>
           </div>
           <div class="xl:w-1/4 px-4">
             <div class="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
-              <img
-                src="https://source.unsplash.com/300x200?cat"
-                alt="game"
-                class="w-full"
-              />
+              <div class="relative">
+                <img
+                  src="https://source.unsplash.com/300x200?color"
+                  alt="game"
+                  class="w-full"
+                />
+                <a
+                  href="!#"
+                  target="_blank"
+                  class="absolute -bottom-4 border-2 border-white right-0  w-10 h-10 mr-3 flex justify-center items-center rounded-full bg-primary hover:border-primary text-white"
+                >
+                  <span>🤍</span>
+                </a>
+              </div>
+
               <div class="px-4 py-6">
-                <h3 class="font-bold text-dark hover:text-primary text-xl mb-5 truncate">
-                  Permainan Takraw
+                <h3 class="font-bold text-dark hover:text-primary text-2xl mb-5 truncate">
+                  Permainan Engklek
                 </h3>
-                <p class="text-base text-secondary mb-8">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Architecto ab fuga enim?
+                <p class="text-lg font-normal text-primary">
+                  <span>🕓</span> Today at 10:00 PM
+                </p>
+                <p class="text-lg font-normal text-slate-400 mb-6">
+                  <span>📌</span> Bandung, West Java
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="xl:w-1/4 px-4">
+            <div class="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
+              <div class="relative">
+                <img
+                  src="https://source.unsplash.com/300x200?color"
+                  alt="game"
+                  class="w-full"
+                />
+                <a
+                  href="!#"
+                  target="_blank"
+                  class="absolute -bottom-4 border-2 border-white right-0  w-10 h-10 mr-3 flex justify-center items-center rounded-full bg-primary hover:border-primary text-white"
+                >
+                  <span>🤍</span>
+                </a>
+              </div>
+
+              <div class="px-4 py-6">
+                <h3 class="font-bold text-dark hover:text-primary text-2xl mb-5 truncate">
+                  Permainan Engklek
+                </h3>
+                <p class="text-lg font-semibold text-primary">
+                  <span>🕓</span> Today at 10:00 PM
+                </p>
+                <p class="text-lg font-semibold text-slate-400 mb-6">
+                  <span>📌</span> Bandung, West Java
                 </p>
               </div>
             </div>
