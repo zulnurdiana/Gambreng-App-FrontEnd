26282ae9b579d57a99a7e63e34dfe80e6c6af986
zulnurdiana <zulnurdiana@gmail.com>
2023-02-01 14:11:43 +0700

    redesign event feature

--- src/components/ListEvent.js
+++ src/components/ListEvent.js
@@ -5,75 +5,75 @@ const ListEvent = () => {
     <div className="min-full-no-navbar pt-24">
       <div class="container">
         <div class="flex flex-wrap px-4">
-          <div class="w-full lg:w-1/2 xl:w-1/3 px-4">
+          <div class="xl:w-1/4 px-4">
             <div class="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
               <img
-                src="https://source.unsplash.com/300x200?programming"
-                alt="programming"
+                src="https://source.unsplash.com/300x200?orange"
+                alt="game"
                 class="w-full"
               />
               <div class="px-4 py-6">
                 <h3 class="font-bold text-dark hover:text-primary text-xl mb-5 truncate">
-                  Permainan Tradisional Congklak
+                  Permainan Congklak
                 </h3>
                 <p class="text-base text-secondary mb-8">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                   Architecto ab fuga enim?
                 </p>
-                <a
-                  href="!#"
-                  class="py-2 px-4 w-44  bg-primary text-sm rounded-lg text-white font-bold hover:opacity-80"
-                >
-                  Baca selengkapnya
-                </a>
               </div>
             </div>
           </div>
-          <div class="w-full lg:w-1/2 xl:w-1/3 px-4">
+          <div class="xl:w-1/4 px-4">
             <div class="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
               <img
-                src="https://source.unsplash.com/300x200?mechanical+keyboard"
-                alt="programming"
+                src="https://source.unsplash.com/300x200?color"
+                alt="game"
                 class="w-full"
               />
               <div class="px-4 py-6">
                 <h3 class="font-bold text-dark hover:text-primary text-xl mb-5 truncate">
-                  Permainan Tradisional Engklek
+                  Permainan Engklek
                 </h3>
                 <p class="text-base text-secondary mb-8">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                   Architecto ab fuga enim?
                 </p>
-                <a
-                  href="!#"
-                  class="py-2 px-4 w-44  bg-primary text-sm rounded-lg text-white font-bold hover:opacity-80"
-                >
-                  Baca selengkapnya
-                </a>
               </div>
             </div>
           </div>
-          <div class="w-full lg:w-1/2 xl:w-1/3 px-4">
+          <div class="xl:w-1/4 px-4">
             <div class="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
               <img
-                src="https://source.unsplash.com/300x200?gaming+gear"
-                alt="programming"
+                src="https://source.unsplash.com/300x200?game"
+                alt="game"
                 class="w-full"
               />
               <div class="px-4 py-6">
                 <h3 class="font-bold text-dark hover:text-primary text-xl mb-5 truncate">
-                  Permainan Tradisional Gundu
+                  Permainan Gundu
                 </h3>
                 <p class="text-base text-secondary mb-8">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                   Architecto ab fuga enim?
                 </p>
-                <a
-                  href="!#"
-                  class="py-2 px-4 w-44  bg-primary text-sm rounded-lg text-white font-bold hover:opacity-80"
-                >
-                  Baca selengkapnya
-                </a>
+              </div>
+            </div>
+          </div>
+          <div class="xl:w-1/4 px-4">
+            <div class="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
+              <img
+                src="https://source.unsplash.com/300x200?cat"
+                alt="game"
+                class="w-full"
+              />
+              <div class="px-4 py-6">
+                <h3 class="font-bold text-dark hover:text-primary text-xl mb-5 truncate">
+                  Permainan Takraw
+                </h3>
+                <p class="text-base text-secondary mb-8">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Architecto ab fuga enim?
+                </p>
               </div>
             </div>
           </div>
