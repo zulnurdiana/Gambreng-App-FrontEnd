7345845eaac0da8db3ae95a9f3758c61dce639f5
zulnurdiana <zulnurdiana@gmail.com>
2023-02-02 18:59:06 +0700

    fix form tambah acara & simulasi

--- src/components/ListEvent.js
+++ src/components/ListEvent.js
@@ -26,7 +26,39 @@ const ListEvent = () => {
               </div>
 
               <div className="px-4 py-6">
-                <h3 className="font-bold text-dark hover:text-primary text-2xl mb-5 truncate">
+                <h3 className="font-bold text-dark text-2xl mb-5 truncate">
+                  Lorem Ipsum
+                </h3>
+                <p className="text-lg font-normal text-primary">
+                  <span>🕓</span> Today at 10:00 PM
+                </p>
+                <p className="text-lg font-normal text-slate-400 mb-6">
+                  <span>📌</span> Bandung, West Java
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="xl:w-1/4 px-4">
+            <div className="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
+              <div className="relative">
+                <NavLink to={"/detailevent"}>
+                  <img
+                    src="https://source.unsplash.com/300x200?color"
+                    alt="game"
+                    className="w-full"
+                  />
+                  <a
+                    href="!#"
+                    target="_blank"
+                    className="absolute  -bottom-4 border-2 border-white right-0  w-10 h-10 mr-3 flex justify-center items-center rounded-full bg-primary hover:border-primary text-white"
+                  >
+                    <span>🤍</span>
+                  </a>
+                </NavLink>
+              </div>
+
+              <div className="px-4 py-6">
+                <h3 className="font-bold text-dark text-2xl mb-5 truncate">
                   Lorem Ipsum
                 </h3>
                 <p className="text-lg font-normal text-primary">
@@ -58,7 +90,7 @@ const ListEvent = () => {
               </div>
 
               <div className="px-4 py-6">
-                <h3 className="font-bold text-dark hover:text-primary text-2xl mb-5 truncate">
+                <h3 className="font-bold text-dark text-2xl mb-5 truncate">
                   Lorem Ipsum
                 </h3>
                 <p className="text-lg font-normal text-primary">
@@ -90,39 +122,7 @@ const ListEvent = () => {
               </div>
 
               <div className="px-4 py-6">
-                <h3 className="font-bold text-dark hover:text-primary text-2xl mb-5 truncate">
-                  Lorem Ipsum
-                </h3>
-                <p className="text-lg font-normal text-primary">
-                  <span>🕓</span> Today at 10:00 PM
-                </p>
-                <p className="text-lg font-normal text-slate-400 mb-6">
-                  <span>📌</span> Bandung, West Java
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="xl:w-1/4 px-4">
-            <div className="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
-              <div className="relative">
-                <NavLink to={"/detailevent"}>
-                  <img
-                    src="https://source.unsplash.com/300x200?color"
-                    alt="game"
-                    className="w-full"
-                  />
-                  <a
-                    href="!#"
-                    target="_blank"
-                    className="absolute -bottom-4 border-2 border-white right-0  w-10 h-10 mr-3 flex justify-center items-center rounded-full bg-primary hover:border-primary text-white"
-                  >
-                    <span>🤍</span>
-                  </a>
-                </NavLink>
-              </div>
-
-              <div className="px-4 py-6">
-                <h3 className="font-bold text-dark hover:text-primary text-2xl mb-5 truncate">
+                <h3 className="font-bold text-dark text-2xl mb-5 truncate">
                   Lorem Ipsum
                 </h3>
                 <p className="text-lg font-normal text-primary">
