e4103c93df114eb61aa8a95f65e8a0302db53f51
taufiqraw <waskitaufiq@gmail.com>
2023-02-03 16:48:15 +0700

    added font-awesome dependency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthProvider";
 
