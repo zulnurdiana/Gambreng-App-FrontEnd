18ad1bc7d6c30af04a0171daeccd2afda5e7326e
taufiqraw <waskitaufiq@gmail.com>
2023-02-04 21:46:25 +0700

    delete edit on event list and minor fix on list

--- src/components/ListEvent.js
+++ src/components/ListEvent.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
 import ReactPaginate from "react-paginate";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
@@ -25,6 +24,11 @@ const ListEvent = () => {
     setPage(page);
   };
 
+  const deleteEvent = async (id) => {
+    await axios.delete(`event/${id}`);
+    getPage();
+  };
+
   useEffect(() => {
     getPage();
   }, [page]);
@@ -32,6 +36,22 @@ const ListEvent = () => {
   return (
     <div className="min-full-no-navbar pt-16">
       <div className="container">
+        <Link
+          to={"/tambah-acara"}
+          className="ml-8 flex items-center w-[15%] font-bold text-white rounded-lg mb-8 px-5 py-2 text-base z-50 bg-primary hover:opacity-80 hover:shadow-lg transition duration-500"
+        >
+          <span className="fill-current mr-3">
+            <svg
+              role="img"
+              width="20"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M18 13.496h-4.501v4.484h-3v-4.484H6v-2.99h4.5V6.021h3.001v4.485H18v2.99zM21 .041H3C1.348.043.008 1.379 0 3.031v17.94c.008 1.65 1.348 2.986 3 2.988h18c1.651-.002 2.991-1.338 3-2.988V3.031c-.009-1.652-1.348-2.987-3-2.99z" />
+            </svg>
+          </span>
+          Tambah Event
+        </Link>
         {events.length === 0 ? (
           <>
             <div className="min-full-no-navbar relative pt-16">
@@ -42,67 +62,57 @@ const ListEvent = () => {
           </>
         ) : (
           <>
-            <NavLink
-              to={"/tambah-acara"}
-              className="ml-8 flex items-center w-[15%] font-bold text-white rounded-lg mb-8 px-5 py-2 text-base z-50 bg-primary hover:opacity-80 hover:shadow-lg transition duration-500"
-            >
-              <span className="fill-current mr-3">
-                <svg
-                  role="img"
-                  width="20"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M18 13.496h-4.501v4.484h-3v-4.484H6v-2.99h4.5V6.021h3.001v4.485H18v2.99zM21 .041H3C1.348.043.008 1.379 0 3.031v17.94c.008 1.65 1.348 2.986 3 2.988h18c1.651-.002 2.991-1.338 3-2.988V3.031c-.009-1.652-1.348-2.987-3-2.99z" />
-                </svg>
-              </span>
-              Tambah Event
-            </NavLink>
-            <div className="flex flex-wrap px-4">
+            <div className="grid xl:grid-cols-4 gap-x-5">
               {events.map((event) => (
-                <Link
-                  to={`/event/${event.id}`}
-                  key={event.id}
-                  className="xl:w-1/4 px-4"
-                >
-                  <div className="rounded-xl shadow-lg overflow-hidden bg-white mb-10">
+                <div className="relative w-full">
+                  <Link
+                    to={`/event/${event.id}`}
+                    key={event.id}
+                    className="rounded-xl border border-slate-300 overflow-hidden bg-white block group hover:shadow-lg transition-all"
+                  >
                     <div className="relative">
                       <div>
                         <img
                           src={`https://gambreng.fajarbuana.my.id/image/${event.image}`}
-                          // src={`https://source.unsplash.com/300x200?color`}
+                          className="object-cover w-full h-48 transform transition-transform group-hover:scale-110"
                           alt="event"
-                          className="w-full h-48"
                         />
-                        <Link
-                        to={`${event.id}/edit`}>
-                          <button
-                            className="absolute -bottom-4 border-2 border-white right-0 w-20 h-10 mr-3 flex justify-center items-center rounded-full bg-primary hover:border-primary text-white">
-                            <i className="fa fa-edit mr-2"></i>Edit
-                          </button>
-                        </Link>
                       </div>
                     </div>
 
-                    <div className="px-4 py-6">
+                    <div className="px-4 py-6 relative bg-white">
                       <h3 className="font-bold text-dark text-2xl mb-5 truncate">
                         {event.title}
                       </h3>
-                      <p className="text-lg font-normal text-primary">
-                        <span>🕓</span> {event.schedule.slice(0, 10)}
+                      <p className="text-lg font-normal flex items-center gap-x-2 text-primary">
+                        <i className="fa-regular fa-clock"></i>
+                        <span>
+                          {event.schedule.slice(0, 10)}
+                        </span> 
                       </p>
-                      <p className="text-lg font-normal text-slate-400 mb-6">
-                        <span>📌</span> {event.location}
+                      <p className="text-lg font-normal text-slate-400 mb-6 flex items-center gap-x-2">
+                        <span className="w-4 justify-center flex">
+                          <i class="fa-solid fa-location-dot"></i> 
+                        </span>
+                        <span>{event.location}</span>
                       </p>
-                      <Link
-                      to={`/hapus-acara/${event.id}`}>
-                          <button className="justify-center bg-red-900 hover:opacity-80 hover:shadow-lg transition duration-500 mx-2 text-white font-bold py-1 px-4 rounded">
-                            <i className="fa fa-trash pr-2"></i> Hapus
-                          </button>
-                      </Link>
                     </div>
+                  </Link>
+                  <div className="absolute bottom-2 right-2 flex items-center gap-x-2">
+                    <Link
+                      to={`${event.id}/edit`}
+                      className="border-2 rounded-lg bg-white border-blue-500 hover:bg-blue-500 text-blue-500 hover:text-white flex items-center justify-center w-10 h-10"
+                    >
+                      <i className="fa fa-edit"></i>
+                    </Link>
+                    <button
+                      className="border-2 rounded-lg bg-white hover border-red-500 hover:bg-red-500 text-red-500 hover:text-white flex items-center justify-center w-10 h-10"
+                      onClick={() => deleteEvent(event.id)}
+                    >
+                      <i className="fa fa-trash"></i>
+                    </button>
                   </div>
-                </Link>
+                </div>
               ))}
             </div>
           </>
--- src/components/ListPermainan.js
+++ src/components/ListPermainan.js
@@ -35,6 +35,32 @@ const ListPermainan = () => {
   return (
     <div className="min-full-no-navbar pt-16">
       <div className="container">
+      <div className="relative">
+          <div className="fixed bottom-11 right-11 w-10 h-10">
+            <button
+              onClick={() => setForum(true)}
+              className="absolute border-2 border-black w-16 h-16 mr-3 flex justify-center items-center rounded-full bg-primary hover:border-primary hover:scale-95 transition duration-500 text-white"
+            >
+              <span className="text-2xl">💬</span>
+            </button>
+          </div>
+        </div>
+        <Link
+          to={"/tambah-simulasi"}
+          className="ml-8 flex items-center w-[17%] font-bold text-white rounded-lg mb-8 px-5 py-2 text-base z-50 bg-primary hover:opacity-80 hover:shadow-lg transition duration-500"
+        >
+          <span className="fill-current mr-3">
+            <svg
+              role="img"
+              width="20"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M18 13.496h-4.501v4.484h-3v-4.484H6v-2.99h4.5V6.021h3.001v4.485H18v2.99zM21 .041H3C1.348.043.008 1.379 0 3.031v17.94c.008 1.65 1.348 2.986 3 2.988h18c1.651-.002 2.991-1.338 3-2.988V3.031c-.009-1.652-1.348-2.987-3-2.99z" />
+            </svg>
+          </span>
+          Tambah Simulasi
+        </Link>
         {games.length === 0 ? (
           <div className="min-full-no-navbar relative pt-16">
             <div className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
@@ -43,32 +69,6 @@ const ListPermainan = () => {
           </div>
         ) : (
           <>
-            <div className="relative">
-              <div className="fixed bottom-11 right-11 w-10 h-10">
-                <button
-                  onClick={() => setForum(true)}
-                  className="absolute border-2 border-black w-16 h-16 mr-3 flex justify-center items-center rounded-full bg-primary hover:border-primary hover:scale-95 transition duration-500 text-white"
-                >
-                  <span className="text-2xl">💬</span>
-                </button>
-              </div>
-            </div>
-            <Link
-              to={"/tambah-simulasi"}
-              className="ml-8 flex items-center w-[17%] font-bold text-white rounded-lg mb-8 px-5 py-2 text-base z-50 bg-primary hover:opacity-80 hover:shadow-lg transition duration-500"
-            >
-              <span className="fill-current mr-3">
-                <svg
-                  role="img"
-                  width="20"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M18 13.496h-4.501v4.484h-3v-4.484H6v-2.99h4.5V6.021h3.001v4.485H18v2.99zM21 .041H3C1.348.043.008 1.379 0 3.031v17.94c.008 1.65 1.348 2.986 3 2.988h18c1.651-.002 2.991-1.338 3-2.988V3.031c-.009-1.652-1.348-2.987-3-2.99z" />
-                </svg>
-              </span>
-              Tambah Simulasi
-            </Link>
             <div className="flex flex-wrap px-4">
               <div className="xl:w-full px-4">
                 {games.map((game) => (
